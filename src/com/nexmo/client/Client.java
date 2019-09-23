983f30c89dce2b10a626fffbec8f879cd9ca322f
Guilherme Tadeu <guitadeu.py@gmail.com>
2019-09-23 17:17:15 -0300

    Corrigindo Indentação no Código

--- src/com/nexmo/client/Client.java
+++ src/com/nexmo/client/Client.java
@@ -10,47 +10,48 @@ import com.nexmo.client.verify.VerifyResponse;
 import com.nexmo.client.verify.VerifyStatus;
 
 public class Client {
+    
     public static void main(String[] args) throws IOException, NexmoClientException {
 
-		/**
-		* Criando o Cliente Nexmo
-		* */
-		NexmoClient client = new NexmoClient.Builder()
-			.apiKey("INSIRA SUA API KEY")
-			.apiSecret("INSIRA SUA API SECRET")
-			.build();
-		VerifyClient verifyClient = client.getVerifyClient();
+        /**
+        * Criando o Cliente Nexmo
+        * */
+	NexmoClient client = new NexmoClient.Builder()
+	    .apiKey("INSIRA SUA API KEY")
+	    .apiSecret("INSIRA SUA API SECRET")
+            .build();
+	VerifyClient verifyClient = client.getVerifyClient();
 
-		/**
-		* Configurando o Telefone (DDI+DDD+TEL), Empresa e
-		* Tamanho do Token (4-6)
-		* */
-		VerifyRequest request = new VerifyRequest("SEU TELEFONE", "O NOME DA SUA EMPRESA");
-		request.setLength(4);
+	/**
+	* Configurando o Telefone (DDI+DDD+TEL), Empresa e
+	* Tamanho do Token (4-6)
+	* */
+	VerifyRequest request = new VerifyRequest("SEU TELEFONE", "O NOME DA SUA EMPRESA");
+	request.setLength(4);
 
-		/**
-		* Enviando a mensagem via SMS
-		* */
-		VerifyResponse verifyResponse = verifyClient.verify(request);
+	/**
+	* Enviando a mensagem via SMS
+	* */
+	VerifyResponse verifyResponse = verifyClient.verify(request);
 
-		System.out.println("Digite o Código Recebido: ");
+	System.out.println("Digite o Código Recebido: ");
 
-		Scanner input = new Scanner(System.in);
-		String code = input.nextLine();
+	Scanner input = new Scanner(System.in);
+	String code = input.nextLine();
 
-		String requestId = verifyResponse.getRequestId();
+	String requestId = verifyResponse.getRequestId();
 
-		/**
-		* Checando se o Token digitado pelo usuário é 
-		* o mesmo enviado por mensagem pela API.
-		* */
-		CheckResponse checkResponse = verifyClient.check(requestId, code);
-		if (checkResponse.getStatus() == VerifyStatus.OK) {
-			System.out.println("SUCESSO!");
-		} else {
-			System.out.println("ERRO!");
-		}
+	/**
+	* Checando se o Token digitado pelo usuário é 
+	* o mesmo enviado por mensagem pela API.
+	* */
+	CheckResponse checkResponse = verifyClient.check(requestId, code);
+	if (checkResponse.getStatus() == VerifyStatus.OK) {
+	    System.out.println("SUCESSO!");
+	} else {
+	    System.out.println("ERRO!");
+	}
 
-		input.close();
+	input.close();
     }
 }
